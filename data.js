856c2b89baadf0b84af0e398716e9dfc4bcf7cdd
Rakshith V <rakshithv9517@gmail.com>
2024-12-14 22:43:57 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+let songs=[
+{
+    name:'Beliver',
+    path:'music/believer.mp3',
+    artist:'Imagine Dragons',
+    cover: 'images/Beliver.jpg'
+},
+{
+    name:'Bones',
+    path:'music/bones.mp3',
+    artist:'Imagine Dragons',
+    cover: 'images/bones.jpg'
+},
+{
+    name:'Until I Found Her',
+    path:'music/foundu.mp3',
+    artist: 'Stephen Sanchez',
+    cover:'images/found her.jpg'
+},
+{
+    name:'Under The Influence',
+    path:'music/influence.mp3',
+    artist: 'Chris Brown',
+    cover:'images/influence.jpg'
+},
+
+]
